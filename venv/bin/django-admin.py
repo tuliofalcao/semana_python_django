c5f5384dc49e21bc59ec53eff410d25ffda3e23c
Túlio Falcão <falcao.tulio@gmail.com>
2020-07-18 00:58:47 -0300

    First Commit: installing Django, Python-decouple, creating .env and .gitignore

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tulio/Documentos/Documentos/Túlio/Programação/Django/Treinaweb/semana_python_django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
